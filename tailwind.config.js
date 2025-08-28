b6651ceb7e09be6aca35aa1537027df31722cee0
santhoshdmedia <devconsoledmedia@gmail.com>
2025-08-28 18:06:02 +0530

    added tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ export default {
       fontFamily: {
         billfont: ["Poppins"],
       },
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px',
+        '4xl': '2100px', // Add this custom breakpoint
+      },
     },
   },
   plugins: [],
